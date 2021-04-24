425250b947432fc7df5d84a1ef54df1238c49e22
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-04-24 23:28:30 +0530

    Update 1.6 - Day 5: Template Literals.cpp

--- 1.6 - Day 5: Template Literals.cpp
+++ 1.6 - Day 5: Template Literals.cpp
@@ -27,3 +27,9 @@ function readLine() {
 /*
  * Determine the original side lengths and return an array:
  * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
