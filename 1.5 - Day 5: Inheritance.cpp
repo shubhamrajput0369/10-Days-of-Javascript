bd93d410b79fbe764de09ec50352f31cdf5a40a6
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-04-24 23:22:43 +0530

    Update 1.5 - Day 5: Inheritance.cpp

--- 1.5 - Day 5: Inheritance.cpp
+++ 1.5 - Day 5: Inheritance.cpp
@@ -25,3 +25,7 @@ class Square extends Rectangle{
 
 if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.stringify([ 'constructor' ])) {
     const rec = new Rectangle(3, 4);
+const sqr = new Square(3);
+    
+    console.log(rec.area());
+    console.log(sqr.area());
