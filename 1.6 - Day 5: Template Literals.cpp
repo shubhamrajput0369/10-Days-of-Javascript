f0225474c78e67c0c72f6ff47192fb01ba885b8d
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-04-24 23:29:47 +0530

    Update 1.6 - Day 5: Template Literals.cpp

--- 1.6 - Day 5: Template Literals.cpp
+++ 1.6 - Day 5: Template Literals.cpp
@@ -48,3 +48,6 @@ function main() {
     
     const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
     
+    console.log((s1 === x) ? s1 : -1);
+    console.log((s2 === y) ? s2 : -1);
+}
