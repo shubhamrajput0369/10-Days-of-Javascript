11a7cf72764c4bcbae109089a7336f3a92d9c1a0
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-05-01 22:58:11 +0530

    Update 1.8 - Day 6: Bitwise Operators.cpp

--- 1.8 - Day 6: Bitwise Operators.cpp
+++ 1.8 - Day 6: Bitwise Operators.cpp
@@ -28,3 +28,8 @@ function readLine() {
 function getMaxLessThanK(n,k){
     let max=0;
     let and;
+    for(let a=1;a<n;a++)
+    for(let b=a+1;b<=n;b++)
+    {
+        let and = a&b;
+        if(and<k && and>max)
