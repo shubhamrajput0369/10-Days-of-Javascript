b4f12bbffe3a5f1563ed4adde29cdb9e4094cbe8
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-05-01 22:59:19 +0530

    Update 1.8 - Day 6: Bitwise Operators.cpp

--- 1.8 - Day 6: Bitwise Operators.cpp
+++ 1.8 - Day 6: Bitwise Operators.cpp
@@ -44,3 +44,8 @@ function main() {
     const q = +(readLine());
     
     for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+        
+        console.log(getMaxLessThanK(n, k));
+    }
+}
